dff9f8f9f24432fde09ce02a05ac2373c295daeb
Aflah03 <aflahmm35@gmail.com>
2024-06-21 23:05:16 +0530

    yea man

--- hello.cpp
+++ hello.cpp
@@ -16,11 +16,17 @@ using namespace std;
 //     cout << myobject.str ;
 // }
 class Car{
+    public :
     string brand;
     int year;
     string model;
 
 };
 int main(){
+    Car car1;
+    car1.model = "M5";
+    car1.brand  = "BMW";
+    car1.year = 2020;
+    cout <<"1: "<<car1.brand<<" "<<car1.model<<" "<< car1.year;
     
 }
