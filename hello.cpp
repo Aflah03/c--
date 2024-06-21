9c079fc88fca23e27373071a71653a59f3bc021b
Aflah03 <aflahmm35@gmail.com>
2024-06-21 22:56:41 +0530

    ok

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main(){
-string food = "mandi";
-string &meal = food;
-cout << meal;
+// int main(){
+// class MyClass{
+//     public:
+//         int num;
+//         string str;
 
-food = "beaf";
-cout << endl << meal;
-cout << endl << &meal;
-   }
-   
+//     };
+//     MyClass myobject;
+//     myobject.num = 15;
+//     myobject.str = "Aflah";
+
+//     cout << myobject.num <<endl;
+//     cout << myobject.str ;
+// }
+class Car{
+    string brand;
+    int year;
+    string model;
+
+};
+int main(){
+    
+}
